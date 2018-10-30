8dbb5bca8f792b7117786123b05ee4ebb6a940ab
Jonathan Engelsma <jengelsma@gmail.com>
2018-10-30 15:07:55 -0400

    Finished HW8.

--- HW3-Solution/HistoryTableViewController.swift
+++ HW3-Solution/HistoryTableViewController.swift
@@ -18,53 +18,157 @@ class HistoryTableViewController: UITableViewController {
     var entries : [Conversion] = []
     var historyDelegate:HistoryTableViewControllerDelegate?
     
+    var tableViewData: [(sectionHeader: String, entries: [Conversion])]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Uncomment the following line to preserve selection between presentations
+//        // self.clearsSelectionOnViewWillAppear = false
+//
+//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.sortIntoSections(entries: self.entries)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func sortIntoSections(entries: [Conversion]) {
+        
+        var tmpEntries : Dictionary<String,[Conversion]> = [:]
+        var tmpData: [(sectionHeader: String, entries: [Conversion])] = []
+        
+        // partition into sections
+        for entry in entries {
+            let shortDate = entry.timestamp.short
+            if var bucket = tmpEntries[shortDate] {
+                bucket.append(entry)
+                tmpEntries[shortDate] = bucket
+            } else {
+                tmpEntries[shortDate] = [entry]
+            }
+        }
+        
+        // breakout into our preferred array format
+        let keys = tmpEntries.keys
+        for key in keys {
+            if let val = tmpEntries[key] {
+                tmpData.append((sectionHeader: key, entries: val))
+            }
+        }
+        
+        // sort by increasing date.
+        tmpData.sort { (v1, v2) -> Bool in
+            if v1.sectionHeader < v2.sectionHeader {
+                return true
+            } else {
+                return false
+            }
+        }
+        
+        self.tableViewData = tmpData
+    }
+
+    
 
     // MARK: - Table view data source
 
+    // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        if let data = self.tableViewData {
+            return data.count
+        } else {
+            return 0
+        }
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return entries.count
+        if let sectionInfo = self.tableViewData?[section] {
+            return sectionInfo.entries.count
+        } else {
+            return 0
+        }
     }
+
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
-        let entry = entries[indexPath.row]
-        cell.textLabel!.text = "\(entry.fromVal) \(entry.fromUnits) = \(entry.toVal) \(entry.toUnits)"
-        cell.detailTextLabel!.text = "\(entry.timestamp.description)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FancyCell", for: indexPath) as! HistoryTableViewCell
+        if let entry = self.tableViewData?[indexPath.section].entries[indexPath.row] {
+            cell.conversionLabel.text = "\(entry.fromVal) \(entry.fromUnits) = \(entry.toVal) \(entry.toUnits)"
+            cell.timestampLabel.text = "\(entry.timestamp.description)"
+            cell.thumbnail.image = UIImage(imageLiteralResourceName: entry.mode == .Volume ? "volume" : "length")
+        }
         return cell
     }
 
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        // use the historyDelegate to report back entry selected to the calculator scene
+//        if let del = self.historyDelegate {
+//            let conv = entries[indexPath.row]
+//            del.selectEntry(entry: conv)
+//        }
+//
+//        // this pops to the calculator
+//        _ = self.navigationController?.popViewController(animated: true)
+//    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // use the historyDelegate to report back entry selected to the calculator scene
         if let del = self.historyDelegate {
-            let conv = entries[indexPath.row]
-            del.selectEntry(entry: conv)
+            if let conv = self.tableViewData?[indexPath.section].entries[indexPath.row] {
+                del.selectEntry(entry: conv)
+            }
         }
         
         // this pops to the calculator
         _ = self.navigationController?.popViewController(animated: true)
     }
 
+    
+
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) ->
+        String? {
+            return self.tableViewData?[section].sectionHeader
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) ->
+        CGFloat {
+            return 80.0
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection
+        section: Int) {
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = BACKGROUND_COLOR
+        header.contentView.backgroundColor = FOREGROUND_COLOR
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView,
+                            forSection section: Int) {
+        let header = view as! UITableViewHeaderFooterView
+        header.textLabel?.textColor = BACKGROUND_COLOR
+        header.contentView.backgroundColor = FOREGROUND_COLOR
+    }
+
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -112,3 +216,27 @@ class HistoryTableViewController: UITableViewController {
     */
 
 }
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+extension Date {
+    struct Formatter {
+        static let short: DateFormatter = {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            return formatter
+        }()
+    }
+    
+    var short: String {
+        return Formatter.short.string(from: self)
+    }
+}
+
+
